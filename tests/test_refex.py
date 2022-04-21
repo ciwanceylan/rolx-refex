68db9b6594f440249e2af69cb00c5ecb82487c33
Ciwan Ceylan <ciwan.ceylan@outlook.com>
2022-04-21 11:58:55 +0200

    Add test for non weights

--- tests/test_refex.py
+++ tests/test_refex.py
@@ -23,22 +23,30 @@ def five_node_graph_adj():
     return nx.adjacency_matrix(graph).T
 
 
-def test_egonet_features(five_node_graph_adj):
-    ego_features = refex.extract_egonet_features(five_node_graph_adj)
+@pytest.mark.parametrize('use_weights', [True, False])
+def test_egonet_features(five_node_graph_adj, use_weights):
+    ego_features = refex.extract_egonet_features(five_node_graph_adj, use_weights=use_weights)
     answer = np.asarray([[6., 3., 3., 6., 3., 3.],
                          [8., 3., 1., 8., 3., 1.],
                          [12., 0., 0., 12., 0., 0.],
                          [7., 3., 2., 7., 3., 2.],
                          [9., 0., 3., 9., 0., 3.]])
-    assert np.allclose(ego_features, answer)
+    if use_weights:
+        assert np.allclose(ego_features, answer)
+    else:
+        assert np.allclose(ego_features, answer[:, :3])
 
 
-def test_node_features(five_node_graph_adj):
-    node_features = refex.extract_node_features(five_node_graph_adj)
+@pytest.mark.parametrize('use_weights', [True, False])
+def test_node_features(five_node_graph_adj, use_weights):
+    node_features = refex.extract_node_features(five_node_graph_adj, use_weights=use_weights)
 
     answer = np.asarray([[3., 0., 3., 0.],
                          [2., 3., 2., 3.],
                          [3., 3., 3., 3.],
                          [1., 3., 1., 3.],
                          [3., 3., 3., 3.]])
-    assert np.allclose(node_features, answer)
+    if use_weights:
+        assert np.allclose(node_features, answer)
+    else:
+        assert np.allclose(node_features, answer[:, :2])
